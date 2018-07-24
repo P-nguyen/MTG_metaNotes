a8960d5e425c0e33021c1415ad5562b2ad1a0ba9
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-07-24 09:34:28 -0700

    added nodejs scraper

--- data/scryfall_axios.js
+++ data/scryfall_axios.js
@@ -1,27 +1,42 @@
 var axios = require('axios');
 var fs = require('fs');
 
+var pageCounter = 643;//now needs to be 901 to 1092`
+var finalPage = 900; //1092
+console.log(pageCounter);
 
 async function scryfallCall (){
 
-    var getRequest = 'https://api.scryfall.com/cards?page=' + 1 + '&format=json';
+    var getRequest = 'https://api.scryfall.com/cards?page=' + pageCounter + '&format=json';
     var response = await axios.get(getRequest);
 
-    //let jsonString = JSON.stringify(response.data.data[0].name);
     let MTG = response.data.data;
+    let finalString = '';
+    for (let i = 0; i < 175; i++){
+        let dataString = `{
+            "id":"${MTG[i].id}",
+            "name":"${MTG[i].name}",
+            "image_uris":${JSON.stringify(MTG[i].image_uris)},
+            "cmc":${MTG[i].cmc},
+            "colors":${JSON.stringify(MTG[i].colors)},
+            "legalities":${JSON.stringify(MTG[i].legalities)}
+        },`
 
-    let dataString = `{
-        "id":"${MTG[80].id}",
-        "name":"${MTG[80].name}",
-        "image_uris":${JSON.stringify(MTG[80].image_uris)},
-        "cmc":${MTG[80].cmc},
-        "colors":${JSON.stringify(MTG[80].colors)},
-        "legalities":${JSON.stringify(MTG[80].legalities)}
-    },`
-
-    fs.appendFile('./testFile4.json', dataString , function (err) {
+        finalString += dataString;
+  
+    }
+    
+    fs.appendFile('./data/mtg_cards3.json', finalString , function (err) {
         if (err) throw err;
     });
+
+    pageCounter++;
+    console.log(pageCounter);
+    if (pageCounter <= finalPage){
+        setTimeout(scryfallCall, 2000);
+    }else{
+        return;
+    }
 
 }
 
@@ -29,6 +44,8 @@ async function scryfallCall (){
 
 //set recursive settimeout to call function again and continue writing.
 //if we hit the  last call at 1092 then return out and finish all calls.
+
+//1-1092 pages.
 
 
 scryfallCall();
